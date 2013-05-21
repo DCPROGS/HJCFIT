da46a913111e9977e713576be1eda0f093ac72c2
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2013-05-21 17:43:49 +0100

    Added derivative calculation. Still no test.

--- likelihood/asymptotes.cc
+++ likelihood/asymptotes.cc
@@ -5,8 +5,12 @@
 
 namespace DCProgs {
 
-  Asymptotes :: Asymptotes   (StateMatrix & _matrix, t_real _tau, bool _doopen=true)
-                           : matrix_(_matrix), tau_(_tau) {
+  // Only the God of linkers knows why we need this declaration twice.
+  constexpr t_real DeterminantEq :: ZERO;
+
+  DeterminantEq :: DeterminantEq   (StateMatrix & _matrix, t_real _tau, bool _doopen)
+                                 : tau_(_tau), matrix_(_matrix), ff_eigenvalues_(),
+                                   ff_eigenvectors_() {
     if(not _doopen) {
       t_int const nopen = matrix_.nopen;
       t_int const nclose = matrix_.matrix.rows() - nopen;
@@ -20,25 +24,35 @@ namespace DCProgs {
       matrix_.ff() = ff;
       matrix_.fa() = fa;
     }
-    std::tie(ff_eigenvalues, ff_eigenvectors) = matrix_.eigenstuff();
+    Eigen::EigenSolver<t_rmatrix> eigsolver(matrix_.ff());
+    if(eigsolver.info() != Eigen::Success) 
+        throw errors::Mass("Could not solve eigenvalue problem.");
+    ff_eigenvalues_ = eigsolver.eigenvalues().real();
+    ff_eigenvectors_ = eigsolver.eigenvectors().real();
   }
 
-  t_rmatrix Asymptotes :: integral_(t_real _s) {
+  t_rmatrix DeterminantEq :: integral_(t_real _s) const {
  
-    t_rvector const alpha = ff_eigenvalues - _s;
-    t_rmatrix diagonal = t_rmatrix::Zero(ff_eigenvalues.size(), ff_eigenvalues.size());
-    for(t_int i(0); i < ff_eigenvalues.size(); ++i) {
-      if(std::abs(alpha(i)) > _zero)
-        diagonal(i, i) =  (std::exp(alpha(i) * tau_) - 1e0) / alpha(i);
-    }
+    t_rvector const alpha = ff_eigenvalues_.array() - _s;
+    auto const is_not_eig = alpha.array().abs() > ZERO;
+    t_rmatrix diagonal = t_rmatrix::Zero(ff_eigenvalues_.size(), ff_eigenvalues_.size());
+    for(t_int i(0); i < ff_eigenvalues_.size(); ++i) 
+      diagonal(i, i) = is_not_eig(i) ?  (std::exp(alpha(i) * tau_) - 1e0) / alpha(i): tau_; 
     return ff_eigenvectors_ * diagonal * ff_eigenvectors_.transpose();
   }
 
-  t_rmatrix Asymptotes :: Sff_star(t_real _s) {
+  t_rmatrix DeterminantEq::s_derivative(t_real _s) const { 
 
-    t_rmatrix result = t_rmatrix::Zero(ff_eigenvalues.size(), ff_eigenvalues.size());
-    result.diagonal() = 1e0 - ((ff_eigenvalues - _s) * tau_).exp();
-    return ff_eigenvectors_ * result * ff_eigenvectors_.transpose();
+    t_rvector const alpha = ff_eigenvalues_.array() - _s;
+    auto const is_not_eig = alpha.array().abs() > ZERO;
+    t_rmatrix diagonal = t_rmatrix::Zero(ff_eigenvalues_.size(), ff_eigenvalues_.size());
+    for(t_int i(0); i < ff_eigenvalues_.size(); ++i) {
+      if(is_not_eig(i)) {
+        t_real const invalpha = 1e0 / alpha(i);
+        diagonal(i, i) = invalpha * ((invalpha - tau_) * std::exp(alpha(i)*tau_) - invalpha);
+      } else diagonal(i, i) = -tau_ * tau_ * 0.5;
+    }
+    return this->id_() +
+           matrix_.af() * ff_eigenvectors_ * diagonal * ff_eigenvectors_.transpose() * matrix_.fa(); 
   }
-
 }
--- likelihood/asymptotes.h
+++ likelihood/asymptotes.h
@@ -2,6 +2,7 @@
 #define DCPROGS_LIKELIHOOD_ASYMPTOTES_H
 
 #include <DCProgsConfig.h>
+#include "state_matrix.h"
 
 namespace DCProgs {
 
@@ -10,7 +11,7 @@ namespace DCProgs {
   //! \detail The whole implementation is done w.r.t. to AF transitions. 
   //!         However, in practice, this is sufficient to compute FA transitions as well, by messing
   //!         with the input matrix.
-  class Asymptotes {
+  class DeterminantEq {
     
     public:
       //! Constructor. 
@@ -18,30 +19,38 @@ namespace DCProgs {
       //!                     \f$^eG_{AF}(t\righarrow\infty)\f$
       //! \param[in] _tau: Missed event resolution.
       //! \param[in] _doopen: Whether to do AF or FA.
-      Asymptotes(StateMatrix & _matrix, t_real _tau, bool _doopen=true);
+      DeterminantEq(StateMatrix & _matrix, t_real _tau, bool _doopen=true);
 
+      //! Computes \f$Q_{AA} + Q_{AF}\ \int_0^\tau e^{-st}e^{Q_{FF}t}\partial\,t\ Q_{FA}\f$
       inline t_rmatrix H(t_real _s) const {
-        return _Qaa + _Qaf * this->integral_(s) * _Qfa;
+        return matrix_.aa() + matrix_.af() * this->integral_(_s) * matrix_.fa();
       }
 
-      inline t_rmatrix operator(t_real _s) const { 
-        return (_s * t_rmatrix::Identity(matrix_.nopen, matrix_.nopen) - H(_s)).determinant();
+      //! Computes the determinant \f$\mathrm{det}(sI - H(s))\f$
+      inline t_real operator()(t_real _s) const { 
+        return (_s * this->id_() - H(_s)).determinant();
       }
-
+      //! Derivative along _s
+      inline t_rmatrix s_derivative(t_real _s) const;
 
     protected:
       //! Computes integral \f$\int_0^\tau\partial\,t\ e^{(Q_{FF} - sI)t}\f$
-      t_rmatrix integral_(t_real _s);
-      //! Computes \f$S_{FF}^{*}=I - e^{(Q_{FF} - sI)\tau}\f$
-      t_rmatrix Sff_star(t_real _s);
+      t_rmatrix integral_(t_real _s) const;
+      //! Just the identity, just to write shorter code.
+      inline auto id_() const ->decltype(t_rmatrix::Identity(1, 1)) 
+        { return t_rmatrix::Identity(matrix_.nopen, matrix_.nopen); }
 
     protected:
+      //! Time below which events are missed
+      t_real tau_;
       //! The transition state matrix on which to act.
       StateMatrix matrix_;
       //! The eigenvalues of the ff matrix. Computed once.
-      t_rvector ff_eigenvalues;
+      t_rvector ff_eigenvalues_;
       //! The eigenvectors of the ff matrix. Computed once.
-      t_rmatrix ff_eigenvectors;
+      t_rmatrix ff_eigenvectors_;
+      //! Hard coded static constant zero.
+      constexpr static t_real ZERO = 1e-12;
   };
 
 }
