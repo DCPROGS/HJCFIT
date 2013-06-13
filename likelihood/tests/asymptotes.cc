cd3123d90702795e6c0a0d332a092d074e7da3a2
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2013-06-13 14:01:44 +0100

    Looks it asymptotic back end works

--- likelihood/tests/asymptotes.cc
+++ likelihood/tests/asymptotes.cc
@@ -24,23 +24,23 @@ class DeterminantEqTest : public ::testing::Test {
 
 //! Convergence criteria for taylor expansion.
 template<class T_APPROX, class T_EXACT> 
-   void taylor_convergence( T_APPROX &_approx, T_EXACT &_exact, 
-                            t_real _s, t_real _tau, t_real _dtau,
-                            std::string const &_message = "", t_real M = 10e0 ) {
-     t_rmatrix const order2 = 0.5 * _dtau * (_exact(_s, _tau + _dtau) - _exact(_s, _tau));
-     t_rmatrix const approx = _approx(_s, _tau + _dtau) - _approx(_s, _tau);
-     t_rmatrix const exact  = _dtau * _exact(_s, _tau);
-     t_rmatrix const diff = approx - exact;
-     t_bmatrix condition =    (diff.array().abs() < M * order2.array().abs())
-                           or (diff.array().abs() < 1e-12);
-     EXPECT_TRUE( condition.all() ) << _message 
-       << "Params:  tau=" << _tau << " -- s=" << _s << " -- dtau=" << _dtau << "\n"
-       << "approx: \n" << approx << "\n"
-       << "exact:  \n" << exact  << "\n"
-       << "diff:  \n" << diff  << "\n"
-       << "condition:  \n" << condition  << "\n"
-       << "max (M=" << M << "):  \n" << (M * order2)  << "\n";
- }
+  void taylor_convergence( T_APPROX &_approx, T_EXACT &_exact, 
+                           t_real _x, t_real _dx,
+                           std::string const &_message = "", t_real M = 10e0 ) {
+    t_rmatrix const order2 = 0.5 * _dx * (_exact(_x + _dx) - _exact(_x));
+    t_rmatrix const approx = _approx(_x + _dx) - _approx(_x);
+    t_rmatrix const exact  = _dx * _exact(_x);
+    t_rmatrix const diff = approx - exact;
+    t_bmatrix condition =    (diff.array().abs() < M * order2.array().abs())
+                          or (diff.array().abs() < 1e-11);
+    EXPECT_TRUE( condition.all() ) << _message 
+      << "Params:  x=" << _x << " -- dx=" << _dx << "\n"
+      << "approx: \n" << approx << "\n"
+      << "exact:  \n" << exact  << "\n"
+      << "diff:  \n" << diff  << "\n"
+      << "condition:  \n" << condition  << "\n"
+      << "max (M=" << M << "):  \n" << (M * order2)  << "\n";
+}
 
 
 // Missed event with t critical == zero (e.g. no missed event)
@@ -94,17 +94,22 @@ TEST_F(DeterminantEqTest, from_tau_derivative) {
 
   StateMatrix const states(Q, 2);
   DeterminantEq determinant(states, 0, false); 
-  auto approx = [&](t_real _s, t_real _tau) { return determinant.H(_s, _tau); };
-  auto exact = [&](t_real _s, t_real _tau) -> t_rmatrix {
-    return states.fa() * std::exp(-_s * _tau) * (_tau * states.aa()).exp() * states.af();
-  };
 
   t_real svec[] = {0e0, 1e-1, 1e0};
   t_real taus[] = {0e0, 1e-4, 1e-3, 1e-2, 1e-1, 1e0};
   t_real dtaus[] = {1e-4, 1e-6, 1e-8};
-  for(t_real s: svec) for(t_real tau: taus) for(t_real dtau: dtaus) {
-    if(dtau >= tau) continue;
-    taylor_convergence(approx, exact, s, tau, dtau, "Testing H\n");
+  for(t_real s: svec) {
+    auto approx = [&determinant, &s](t_real _tau) { return determinant.H(s, _tau); };
+    auto exact = [&states, &s](t_real _tau) -> t_rmatrix {
+      return states.fa() * std::exp(-s * _tau) * (_tau * states.aa()).exp() * states.af();
+    };
+    std::ostringstream sstr;
+    sstr << "Testing H with s=" << s << "\n";
+    std::string const message = sstr.str();
+
+    for(t_real tau: taus)
+      for(t_real dtau: dtaus) 
+        taylor_convergence(approx, exact, tau, dtau, sstr.str());
   }
 }
 
@@ -113,21 +118,53 @@ TEST_F(DeterminantEqTest, s_derivative_from_tau_derivative) {
 
   StateMatrix const states(Q, 2);
   DeterminantEq determinant(states, 0, false); 
-  auto approx = [&determinant](t_real _s, t_real _tau) {
-    return determinant.s_derivative(_s, _tau); 
-  };
-  auto exact = [&states](t_real _s, t_real _tau) -> t_rmatrix {
-    return -_tau * std::exp(-_s * _tau) * states.fa() * (_tau * states.aa()).exp() * states.af();
-  };
 
   t_real svec[] = {0e0, 1e-1, 1e0};
   t_real taus[] = {0e0, 1e-4, 1e-3, 1e-2, 1e-1, 1e0};
   t_real dtaus[] = {1e-4, 1e-6, 1e-8};
-  for(t_real s: svec) for(t_real tau: taus) for(t_real dtau: dtaus) {
-    if(dtau >= tau) continue;
-    taylor_convergence(approx, exact, s, tau, dtau, "Testing s_derivative\n");
+  for(t_real s: svec) {
+    auto approx = [&determinant, &s](t_real _tau) { return determinant.s_derivative(s, _tau); };
+    auto exact = [&states, &s](t_real _tau) -> t_rmatrix {
+      return -_tau * std::exp(-s * _tau) * states.fa() * (_tau * states.aa()).exp() * states.af();
+    };
+    
+    std::ostringstream sstr;
+    sstr << "Testing s_derivative with s=" << s << "\n";
+    std::string const message = sstr.str();
+
+    for(t_real tau: taus)
+      for(t_real dtau: dtaus) 
+        taylor_convergence(approx, exact, tau, dtau, sstr.str());
   }
 }
+
+// Test s derivative from numerical derivative of H.
+TEST_F(DeterminantEqTest, s_derivative_from_H) {
+
+  StateMatrix const states(Q, 2);
+  DeterminantEq determinant(states, 0, false); 
+
+  t_real taus[] = {0e0, 1e-4, 1e-3, 1e-2, 1e-1, 1e0};
+  t_real svec[] = {0e0, 1e-4, 1e-3, 1e-2, 1e-1, 1e0};
+  t_real dsvec[] = {1e-4, 1e-6, 1e-8};
+  for(t_real tau: taus) {
+
+    auto approx = [&determinant, &tau](t_real _s) { return determinant.H(_s, tau); };
+    auto exact = [&determinant, &tau](t_real _s) { 
+      t_rmatrix const result = determinant.s_derivative(_s, tau); 
+      return result - t_rmatrix::Identity(result.rows(), result.cols());
+    };
+    
+    std::ostringstream sstr;
+    sstr << "Testing s_derivative with tau=" << tau << "\n";
+    std::string const message = sstr.str();
+
+    for(t_real s: svec)
+      for(t_real ds: dsvec) 
+        taylor_convergence(approx, exact, s, ds, sstr.str());
+  }
+}
+
 
 
 int main(int argc, char **argv) {
