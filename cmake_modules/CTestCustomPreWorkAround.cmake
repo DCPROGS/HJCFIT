58e036fc59b2facd865bee899f7fc53f6f7a23e6
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2013-06-26 09:48:51 +0100

    Problem executing pre-test scripts
      Newer cmake versions do not allow pre-test scripts to in CTestCustom.cmake to
    take parameters, at least on Unices. Seems to work on windows. The fall-back at this point it to create a fake c program that shells out and executes the command...
    If this is really only on Unices, might be better to create a bash script.

--- cmake_modules/CTestCustomPreWorkAround.cmake
+++ cmake_modules/CTestCustomPreWorkAround.cmake
@@ -1,6 +1,6 @@
-if(APPLE)
-  # Newer cmakes can't cope with CTEST_CUSTOM_PRE_TEST having arguments, at least on Mac.
-  # Hence the BS below.
+if(NOT WIN32)
+  # Newer cmakes can't cope with CTEST_CUSTOM_PRE_TEST having arguments, at
+  # least on Mac and Linux. Hence the BS below.
   file(WRITE ${CMAKE_BINARY_DIR}/fakeinstall.cc
        "// Workaround for CTEST_CUSTOM_POST_TEST not allowing arguments \n"
        "#include <stdlib.h> \n"
@@ -15,9 +15,9 @@ if(APPLE)
 
   file(WRITE ${CMAKE_BINARY_DIR}/CTestCustom.cmake
     "set(CTEST_CUSTOM_PRE_TEST ${CMAKE_BINARY_DIR}/fake_test_install)\n")
-else(APPLE)
+else(NOT WIN32)
   file(WRITE ${CMAKE_BINARY_DIR}/CTestCustom.cmake
        "set(CTEST_CUSTOM_PRE_TEST \"\\\"${CMAKE_COMMAND}\\\""
-             "  -DCMAKE_INSTALL_PREFIX=${TEST_INSTALL_DIRECTORY}"
-             "  -P \\\"${CMAKE_BINARY_DIR}/cmake_install.cmake\\\"\")\n")
-endif(APPLE)
+  	   "  -DCMAKE_INSTALL_PREFIX=${TEST_INSTALL_DIRECTORY}"
+  	   "  -P \\\"${CMAKE_BINARY_DIR}/cmake_install.cmake\\\"\")\n")
+endif(NOT WIN32)
